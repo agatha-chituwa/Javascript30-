d30d0430625e6dff51447e71b8e38cad6227e3cc
agatha-chituwa <bsc-com-06-18@cc.ac.mw>
2024-04-23 16:17:21 +0200

    solved the 2635 on leetcode

---
+++ leetcode2704/2635.js
@@ -0,0 +1,21 @@
+let fn = function (arr, i = 0) {
+  return i + arr;
+};
+
+var map = function (arr, fn) {
+  let returnedArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    returnedArray.push(fn(arr[i], 1));
+  }
+  return returnedArray;
+};
+
+// var map = function (arr, fn) {
+//  let returnedArray = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     returnedArray.push(fn(arr[i], i));
+//   }
+//   return returnedArray;
+// };
+
+console.log(map([10, 20, 30], fn));
