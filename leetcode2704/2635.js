3cc3a91a158c57f306986b31614201370ede69b1
agatha-chituwa <bsc-com-06-18@cc.ac.mw>
2024-04-24 22:52:49 +0200

    solving this problem Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
    The fn function takes one or two arguments:

--- leetcode2704/2635.js
+++ leetcode2704/2635.js
@@ -5,17 +5,17 @@ let fn = function (arr, i = 0) {
 var map = function (arr, fn) {
   let returnedArray = [];
   for (let i = 0; i < arr.length; i++) {
-    returnedArray.push(fn(arr[i], 1));
+    returnedArray[i] = fn(arr[i], 1);
   }
   return returnedArray;
 };
 
-// var map = function (arr, fn) {
-//  let returnedArray = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     returnedArray.push(fn(arr[i], i));
-//   }
-//   return returnedArray;
-// };
+var map = function (arr, fn) {
+  let returnedArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    returnedArray.push(fn(arr[i], i));
+  }
+  return returnedArray;
+};
 
-console.log(map([10, 20, 30], fn));
+console.log(map([10, 20, 30]));
